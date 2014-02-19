c4df25b6cdd010fd23b66ac2bb51df1e85668a9c
lucasbcoelhoGitHub <lucasbcoelho@gmail.com>
2014-02-19 15:38:25 -0300

    Atualização Mobile v1.0

---
+++ Mobile/CooPizza/src/com/example/coopizza/ListActivity.java
@@ -0,0 +1,42 @@
+package com.example.coopizza;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ListActivity extends Activity {
+
+	private final String TAG = "cooPizza";
+	private ListView listPedidos; 
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+				
+		
+		// Restore any saved state 
+		super.onCreate(savedInstanceState);
+		
+		// Set content view
+		setContentView(R.layout.activity_list);
+		
+		// Initialize UI elements
+		listPedidos = (ListView) findViewById(R.id.listView1);
+		
+		
+		// Link UI elements to actions in code	
+		
+			
+		
+		
+		
+	}
+
+	
+
+}
--- Mobile/CooPizza/src/com/example/coopizza/MainActivity.java
+++ Mobile/CooPizza/src/com/example/coopizza/MainActivity.java
@@ -2,21 +2,62 @@ package com.example.coopizza;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	private final String TAG = "cooPizza";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+				
+		
+		// Restore any saved state 
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		// Set content view
+		setContentView(R.layout.activity_list);
+		
+		// Initialize UI elements
+		//final Button routeBtn = (Button) findViewById(R.id.buttonRoute);
+		final Button listBtn = (Button) findViewById(R.id.buttonList);
+		
+		// Link UI elements to actions in code	
+		listBtn.setOnClickListener(new Button.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {				
+				startListActivity();				
+			}		
+			
+		});
+		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	
+	//Start the ListActivity
+	
+	private void startListActivity() {
+		try {
+			Intent listIntent = new Intent(this, ListActivity.class);
+			startActivity(listIntent);
+			
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+		}
 	}
-
+	
+	//Start the RouteActivity
+	/*
+	private void startRouteActivity() {
+		try {
+			
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+		}
+	}
+	*/
+	
 }
