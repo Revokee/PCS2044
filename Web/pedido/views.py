2416858ac86b4666bee221508608a049f9f09599
Cassius Puodzius <puodzius@gmail.com>
2014-03-08 03:43:56 -0300

    Inclusão do diagrama de classe do módulo de entrega

--- Web/pedido/views.py
+++ Web/pedido/views.py
@@ -9,15 +9,6 @@ from pedido.forms import *
 from pedido.models import *
 from pygeocoder import Geocoder
 
-from pyvirtualdisplay import Display
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-from unidecode import unidecode
-import lxml.html
-
 # Funcao para cadastrar um pedido
 @csrf_exempt 
 def create_pedido(request):
@@ -51,70 +42,3 @@ def delete_pedido(request, pedido_id):
 def index(request):
 	orders = Order.objects.values()
 	return render_to_response('pedidos.html', locals(), context_instance=RequestContext(request))
-
-class OrderManager():
-
-	def __init__(self, home):
-		self.home = unidecode(unicode(home, 'utf-8'))
-
-	def _get_webdriver(self):
-		driver = None
-		i = 0
-		while driver == None:
-			if i == 0:
-				try:
-					driver = webdriver.Chrome()
-					print "Chrome webdriver selected"
-				except Exception:
-					pass
-			elif i == 1:
-				try:
-					driver = webdriver.Firefox()
-					print "Firefox webdriver selected"
-				except Exception:
-					pass
-			elif i == 2:
-				driver = webdriver.Ie()
-				print "Internet Explorer webdriver selected"
-			i += 1
-		return driver
-
-	def bestRoute(self, addresses):
-		for i in range(len(addresses)):
-			addresses[i] = unidecode(unicode(addresses[i], 'utf-8'))
-		download = True
-		if download:	
-			display = Display(visible=0, size=(800, 600))
-			display.start()
-			driver = self._get_webdriver()
-			url = "http://www.routexl.com/?q="
-			url += self.home + "$"
-			for address in addresses:
-				url += address + "$"
-			url = url[:-1]
-			print url
-			driver.get(url)
-			try:
-				WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, 'performance')))
-			except Exception:
-				print "failed (timeout)"
-				return None
-			page = driver.page_source.encode('utf-8')
-		else:
-			page = open("testpage").read()
-		#print page
-		html = lxml.html.fromstring(page)
-		route = {"time": None, "distance": None, "addresses": []}
-		for element in html.xpath('//div[@id="route-canvas"]//b'):
-			#print element.text
-			route["addresses"].append(element.text)
-		params = route["addresses"][-1][route["addresses"][-1].rfind("time"):].split()
-		route["time"] = params[1] + " " + params[2]
-		route["distance"] = params[4] + " " + params[5]
-		del route["addresses"][0]
-		del route["addresses"][-1]
-		del route["addresses"][-1]
-		if download:
-			driver.quit()
-			display.stop()
-		return route
