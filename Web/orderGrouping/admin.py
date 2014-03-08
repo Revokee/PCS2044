69c942aaa01ffce4c220922d78a26131942cbe8d
Cassius Puodzius <puodzius@gmail.com>
2014-03-08 02:15:12 -0300

    Remoção de app obsoleto

--- Web/orderGrouping/admin.py
+++
@@ -1,7 +0,0 @@
-from orderGrouping.models import Order
-from django.contrib import admin
-
-class OrderAdmin(admin.ModelAdmin):
-	list_display= ('latitude', 'longitude')
-
-admin.site.register(Order, OrderAdmin)
