796a751e7b57f0591ef7982bf05ed8187bf2621f
Cassius Puodzius <puodzius@gmail.com>
2014-03-07 23:09:35 -0300

    Inclusão de método para cálculo da melhor rota
    O endereço da pizzaria é informado na construção do objeto da classe OrderManager e com o método best route, uma lista dos endereços de entrega é ordenada levando em conta a melhor rota. Essa ordenação está levando aproximadamente 50s em média para ser concluída, independentemente do número de endereços (testado de 1 a 10 endereços de entrega). A lista é devolvida em uma lista de dicionário (hash list) contendo (supondo que o retorno da função foi recebido na variável "route"):
    
    	route["addresses"]: lista ordenada dos endereços de entrega
    	route["time"]: tempo estimado de entrega (com retorno à pizzaria)
    	route["distance"]: distancia estimada a percorrer para efetuar todas as entregas (com retorno à pizzaria)

--- Web/orderGrouping/views.py
+++ Web/orderGrouping/views.py
@@ -7,6 +7,81 @@ from django.http import HttpResponseRedirect, HttpResponse
 from orderGrouping.forms import *
 from orderGrouping.models import *
 
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from unidecode import unidecode
+import lxml.html
+
+class OrderManager():
+
+	def __init__(self, home):
+		self.home = unidecode(unicode(home, 'utf-8'))
+
+	def _get_webdriver(self):
+		driver = None
+		i = 0
+		while driver == None:
+			if i == 0:
+				try:
+					driver = webdriver.Chrome()
+					print "Chrome webdriver selected"
+				except Exception:
+					pass
+			elif i == 1:
+				try:
+					driver = webdriver.Firefox()
+					print "Firefox webdriver selected"
+				except Exception:
+					pass
+			elif i == 2:
+				driver = webdriver.Ie()
+				print "Internet Explorer webdriver selected"
+			i += 1
+		return driver
+
+	def bestRoute(self, addresses):
+		for i in range(len(addresses)):
+			addresses[i] = unidecode(unicode(addresses[i], 'utf-8'))
+		download = True
+		if download:	
+			display = Display(visible=0, size=(800, 600))
+			display.start()
+			driver = self._get_webdriver()
+			url = "http://www.routexl.com/?q="
+			url += self.home + "$"
+			for address in addresses:
+				url += address + "$"
+			url = url[:-1]
+			print url
+			driver.get(url)
+			try:
+				WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, 'performance')))
+			except Exception:
+				print "failed (timeout)"
+				return None
+			page = driver.page_source.encode('utf-8')
+		else:
+			page = open("testpage").read()
+		#print page
+		html = lxml.html.fromstring(page)
+		route = {"time": None, "distance": None, "addresses": []}
+		for element in html.xpath('//div[@id="route-canvas"]//b'):
+			#print element.text
+			route["addresses"].append(element.text)
+		params = route["addresses"][-1][route["addresses"][-1].rfind("time"):].split()
+		route["time"] = params[1] + " " + params[2]
+		route["distance"] = params[4] + " " + params[5]
+		del route["addresses"][0]
+		del route["addresses"][-1]
+		del route["addresses"][-1]
+		if download:
+			driver.quit()
+			display.stop()
+		return route
 
 # Funcao para cadastrar um pedido
 def create_order(request):
@@ -28,7 +103,6 @@ def delete_order(request, order_id):
 	order.delete()
 	messages.warning(request,'Pedido Deletado com sucesso')
 	return HttpResponseRedirect('/pedidos/')
-
 
 #Funcao para listar todos os pedidos
 def index(request):
