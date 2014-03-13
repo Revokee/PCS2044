a4493770873e76d7939ed36da2fa815a815c4c8a
Márcio Tedesco <marcio.tedesco@gmail.com>
2014-03-13 14:56:28 -0300

    Atualização algoritmo de clustering

--- Web/pedido/controller.py
+++ Web/pedido/controller.py
@@ -78,4 +78,39 @@ class OrderController:
 	def clustering(self, addresses):
 		#Clustering:recebe uma lista de endereços no formato de pares (latitude,longitude) e 			#gera uma lista de listas com os pares (latitude, longitude) que podem ser agrupados
 		#e serem envolvidos em uma mesma entrega
-		return addresses
+		#Recebe uma lista de endereços e a partir dessa lista de endereços,
+		#gera uma lista de listas de endereços 
+		#Verificar a possibilidade de utilizar culsteres utilizando alguma ferramenta 			#disponível (GoogleMaps...) para determinar os pontos de entrega próximos.
+		#Planejamento: entre dois pontos temos a hipotenusa.
+ 		for address in addresses:
+			latitude = address[0]
+			longitude = address[1]
+			addresses[i] = unidecode(addresses[i])
+		download = True
+		# Conversao deConvert latitude and longitude to 
+	        # spherical coordinates in radians.
+    		degrees_to_radians = math.pi/180.0
+        
+   	        # phi = 90 - latitude
+	        phi1 = (90.0 - lat1)*degrees_to_radians
+		phi2 = (90.0 - lat2)*degrees_to_radians
+        
+	        # theta = longitude
+    		theta1 = long1*degrees_to_radians
+    		theta2 = long2*degrees_to_radians
+        
+    		# Cálculo da distância esférico a partir das coordenadas esféricas
+        
+    		# Para duas localizações em coordenadas esféricas 
+ 		# (1, theta, phi) e (1, theta, phi)
+    		# cosine( arc length ) = 
+    		#    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+    		# distance = rho * arc length
+    
+	        cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + 
+        	   math.cos(phi1)*math.cos(phi2))
+ 	        arc = math.acos( cos )
+
+    		# Remember to multiply arc by the radius of the earth 
+    		# in your favorite set of units to get length.
+    		return arc*6373
